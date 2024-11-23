54fd19f8127c81d7c9d752917f57069f6acdf162
Alex Su <suziyue@gmail.com>
2024-11-23 03:10:33 -0800

    Implemented exceptions and try catch Left with test_except_div
    Attempting to refactor to python try

--- interpreterv4.py
+++ interpreterv4.py
@@ -13,6 +13,7 @@ class Interpreter(InterpreterBase):
         super().__init__(console_output, inp)   # call InterpreterBase's constructor
         self.scope_manager = ScopeManager()
         self.ret_flag = False
+        self.exception = None
 
     def run(self, program: str) -> None:
         """
@@ -23,6 +24,11 @@ class Interpreter(InterpreterBase):
         del ast
         # get_func_node guaranteed to return list with at least 1 element
         self.run_func(self.get_func_nodes("main")[0], [])
+        if self.exception is not None:
+            super().error(
+                ErrorType.FAULT_ERROR,
+                f"Uncaught exception: '{self.exception}'"
+            )
 
     def do_func_defs(self, ast: Element) -> None:
         """
@@ -90,9 +96,9 @@ class Interpreter(InterpreterBase):
             super().error(ErrorType.NAME_ERROR, f"Undefined variable '{target_var_name}'")
         expression_node = statement_node.get("expression")
         # We bind the lazily evaluated expression to the variable.
-        scope[target_var_name], _ = self.get_expression_lazy(expression_node)
+        scope[target_var_name] = self.get_expression_lazy(expression_node)
         # When we do an assignment, we indicate that this expression may be evaluated multiple times and therefore, should be cached. We also invalidate any prior cached value
-        setattr(scope[target_var_name], "cached_val", None)
+        scope[target_var_name].cached_val = None
 
     def do_func_call(self, statement_node: Element) -> Element | Value:
         """
@@ -109,7 +115,10 @@ class Interpreter(InterpreterBase):
                 case 0:
                     pass
                 case 1:
-                    super().output(str(self.evaluate_expression(args[0])))
+                    evaluated_expr = self.evaluate_expression(args[0])
+                    if self.exception is not None:
+                        return Value("nil", None)
+                    super().output(str(evaluated_expr))
                 case other:
                     super().error(
                         ErrorType.NAME_ERROR,
@@ -120,13 +129,18 @@ class Interpreter(InterpreterBase):
             else:
                 return Value("string", super().get_input())
         elif name == "print":
-            super().output("".join([str(self.evaluate_expression(x)) for x in args]))
+            evaluated_exprs = []
+            for arg in args:
+                evaluated_exprs.append(self.evaluate_expression(arg))
+                if self.exception is not None:
+                    return Value("nil", None)
+            super().output("".join(map(str, evaluated_exprs)))
             return Value("nil", None)
         else:  # for user defined functions, we pass in the args but do not evaluate them.
             # try all the functions we find, execute the first one that matches the args
             for func in self.get_func_nodes(name):
                 # get_func_node guaranteed to return at least 1 element list
-                bound_args = [self.get_expression_lazy(x)[0] for x in args]
+                bound_args = [self.get_expression_lazy(x) for x in args]
                 retval, error = self.run_func(func, bound_args)
                 if error is None:
                     return retval
@@ -141,6 +155,8 @@ class Interpreter(InterpreterBase):
         2. Run either one of the 2 statement blocks.
         """
         evaluated_expr = self.evaluate_expression(statement_node.get("condition"))
+        if self.exception is not None:
+            return None
         if evaluated_expr.type != "bool":
             super().error(ErrorType.TYPE_ERROR, "Expression must be of type 'bool'")
 
@@ -167,8 +183,12 @@ class Interpreter(InterpreterBase):
         It is assumed that the init and update statements will not set the return flag under any circumstances.
         """
         self.run_statement(statement_node.get("init"))
+        if self.exception is not None:
+            return None
         while True:
             evaluated_expr = self.evaluate_expression(statement_node.get("condition"))
+            if self.exception is not None:
+                break
             if evaluated_expr.type != "bool":
                 super().error(ErrorType.TYPE_ERROR, "Expression must be of type 'bool'")
             if not evaluated_expr.data:
@@ -180,9 +200,38 @@ class Interpreter(InterpreterBase):
             # Pop the scope we pushed
             self.scope_manager.pop()
 
+            if self.exception is not None:
+                break
             if self.ret_flag:
                 return retval
             self.run_statement(statement_node.get("update"))
+            if self.exception is not None:
+                break
+    
+    def do_try_statement(self, statement_node: Element) -> Optional[Element]:
+        """
+        Executes a try statement.
+        1. Push a new block level scope for the statement block.
+        2. Execute the try statement block.
+        3. If an exception is raised in the block by a raise statement, look in the catchers for a suitable handler.
+        4. If found, clear the exception and handle it.
+        """
+        self.scope_manager.push(False)
+        retval = self.run_statement_block(statement_node.get("statements"))
+        self.scope_manager.pop()
+
+        if self.exception is not None:
+            for catcher in statement_node.get("catchers"):
+                if catcher.get("exception_type") != self.exception.data:
+                    continue
+                self.exception = None
+                self.scope_manager.push(False)
+                retval = self.run_statement_block(catcher.get("statements"))
+                self.scope_manager.pop()
+                break
+        
+        if self.ret_flag:
+            return retval
 
     def do_return_statement(self, statement_node: Element) -> Optional[Element]:
         """
@@ -194,9 +243,25 @@ class Interpreter(InterpreterBase):
         Otherwise, the function will not finish executing its statements.
         """
         expr = statement_node.get("expression")
-        retval = None if expr is None else self.get_expression_lazy(expr)[0]
+        retval = None if expr is None else self.get_expression_lazy(expr)
         self.ret_flag = True
         return retval
+    
+    def do_raise_statement(self, statement_node: Element) -> None:
+        """
+        Raises an exception. Evaluates the raise expression and stores it.
+        This also serves as an exception flag.
+        If the evaluation of the expression causes an exception to be raised,
+        then do not set the exceptionand raise that instead.
+        """
+        exception = self.evaluate_expression(statement_node.get("exception_type"))
+        if exception.type != "string":
+            super().error(
+                ErrorType.TYPE_ERROR,
+                f"Expected expression of type 'string' in raise statement, got '{exception.type}': '{exception.data}'"
+            )
+        if self.exception is None:
+            self.exception = exception
 
     def run_statement(self, statement_node: Element) -> Optional[Element]:
         """
@@ -213,19 +278,26 @@ class Interpreter(InterpreterBase):
                 return self.do_if_statement(statement_node)
             case "for":
                 return self.do_for_statement(statement_node)
+            case "try":
+                return self.do_try_statement(statement_node)
             case "return":
                 return self.do_return_statement(statement_node)
+            case "raise":
+                self.do_raise_statement(statement_node)
 
     def run_statement_block(self, statement_block: list[Element]) -> Optional[Element]:
         """
         Runs a block of statements.
         After every statement is executed, check the return flag.
+        Also check if an exception has been raised by the statement.
         If it has been set, return the value immediately.
         """
         if statement_block is None:
             return
         for statement_node in statement_block:
             retval = self.run_statement(statement_node)
+            if self.exception is not None:
+                break
             if self.ret_flag:
                 return retval
 
@@ -257,6 +329,8 @@ class Interpreter(InterpreterBase):
         """
         # Evaluate the first operand
         op1 = self.evaluate_expression(expression_node.get("op1"))
+        if self.exception is not None:
+            return Value("nil", None)
         # If the operation is logical and/or, do shortcircuiting
         if op1.type == "bool" and (expression_node.elem_type == "&&" or expression_node.elem_type == "||"):
             if op1.data and expression_node.elem_type == "||":
@@ -265,6 +339,8 @@ class Interpreter(InterpreterBase):
                 return Value("bool", False)
         # If shortcircuiting failed, or some other operation, evaluate the 2nd operand and proceed
         op2 = self.evaluate_expression(expression_node.get("op2"))
+        if self.exception is not None:
+            return Value("nil", None)
         op = get_binary_operator(op1, op2, expression_node.elem_type)
         if op is None:
             super().error(
@@ -278,6 +354,8 @@ class Interpreter(InterpreterBase):
         Evaluates the operand then performs the correct unary operation on it
         """
         op1 = self.evaluate_expression(expression_node.get("op1"))
+        if self.exception is not None:
+            return Value("nil", None)
         op = get_unary_operator(op1, expression_node.elem_type)
         if op is None:
             super().error(
@@ -286,7 +364,7 @@ class Interpreter(InterpreterBase):
             )
         return op(op1)
     
-    def get_expression_lazy(self, expression_node: Element) -> tuple[Element, Optional[tuple[ErrorType, str]]]:
+    def get_expression_lazy(self, expression_node: Element) -> Element:
         """
         Resolve the expression by finding the variable referred to by the expression,
         Or if a function call or operator, resolve the expressions in the arguments or operands.
@@ -298,31 +376,23 @@ class Interpreter(InterpreterBase):
             # If a variable node, we replace the expression node with the expression bound to the variable
             expression_node, err = self.get_value_of_variable(expression_node)
         else:
-            # We need a deepcopy of the expression node because if required, we need to rebind the expression on repeated assignments
+            # We need a deepcopy of the expression node because, we might need to rebind the expression on repeated assignments, so we can't affect the "template" that we create the expression graph from
             expression_node = deepcopy(expression_node)
             if "op1" in expression_node.dict:
                 # If an operator, we bind the expressions to the operands
-                expr, err = self.get_expression_lazy(expression_node.get("op1"))
-                if err is None:
-                    expression_node.dict["op1"] = expr
-                    if "op2" in expression_node.dict:
-                        expr, err = self.get_expression_lazy(expression_node.get("op2"))
-                        if err is None:
-                            expression_node.dict["op2"] = expr
+                expression_node.dict["op1"] = self.get_expression_lazy(expression_node.get("op1"))
+                if "op2" in expression_node.dict:
+                    expression_node.dict["op2"] = self.get_expression_lazy(expression_node.get("op2"))
             elif expression_node.elem_type == "fcall":
                 # If a function call, we bind the expressions to the arguments
                 for i in range(len(expression_node.get("args"))):
-                    expr, err = self.get_expression_lazy(expression_node.get("args")[i])
-                    if err is not None:
-                        break
-                    expression_node.get("args")[i] = expr
+                    expression_node.get("args")[i] = self.get_expression_lazy(expression_node.get("args")[i])
         # If any error, this would be a name error from an undefined variable. We don't want to error immediately, so set the error on the expression node.
         # When we eventually do evaluate this expression node, then we throw the error.
         if err is not None:
             expression_node.error = err
-            return expression_node, err
         # If a value node, this will not mutate, so we don't need to bind, just return the node
-        return expression_node, None
+        return expression_node
 
     def evaluate_expression(self, expression_node: Optional[Element]) -> Value:
         """
@@ -359,6 +429,8 @@ class Interpreter(InterpreterBase):
         elif expression_node.elem_type == "fcall":
             # Built in functions return values immediately
             result = self.do_func_call(expression_node)
+            if self.exception is not None:
+                return Value("nil", None)
             # User-defined functions return lazy expressions
             if not isinstance(result, Value):
                 result = self.evaluate_expression(result)
